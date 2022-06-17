7a4a5c6e4dc5afb8f800223f2517f8042560f7e5
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-06-17 16:29:40 +0200

    Try using same width ports RAM

--- projects/cicero-cyclone/cicero-rtl/memories/bram.sv
+++ projects/cicero-cyclone/cicero-rtl/memories/bram.sv
@@ -1,56 +1,59 @@
 
-module bram #(  
-        parameter READ_WIDTH            = 64,
-        parameter READ_ADDR_WIDTH       = 9,
-        parameter WRITE_WIDTH           = 32,
-        parameter WRITE_ADDR_WIDTH      = 10
+module bram #(
+        parameter WIDTH                 = 64,
+		  parameter ADDR_WIDTH            = 10
+//        parameter READ_WIDTH            = 64,
+//        parameter READ_ADDR_WIDTH       = 9,
+//        parameter WRITE_WIDTH           = 32,
+//        parameter WRITE_ADDR_WIDTH      = 10
         )
 
         (
         input                         clk    ,
         input                         rst  ,
         input                         r_valid,
-        input  [READ_ADDR_WIDTH-1 :0] r_addr ,
-        output [READ_WIDTH-1      :0] r_data ,
-        input  [WRITE_ADDR_WIDTH-1:0] w_addr ,
+        input  [ADDR_WIDTH-1:0]       r_addr ,
+        output reg [WIDTH-1:0]            r_data ,
+        input  [ADDR_WIDTH-1:0]       w_addr ,
         input                         w_valid,
-        input  [WRITE_WIDTH-1     :0] w_data 
+        input  [WIDTH-1:0]            w_data 
         );
 
-      `define max(a,b) ((a) > (b) ? (a) : (b))
-      `define min(a,b) ((a) < (b) ? (a) : (b))
-
-      function integer log2; 
-        input integer value; reg [31:0] shifted; 
-        integer res; 
-      begin 
-        if (value < 2) log2 = value; 
-        else 
-          begin 
-            shifted = value-1;
-            for (res=0; shifted>0; res=res+1) 
-              shifted = shifted>>1; 
-            log2 = res;
-          end 
-      end 
-      endfunction
-
-      localparam READ_SIZE  = 2**READ_ADDR_WIDTH;
-      localparam WRITE_SIZE = 2**WRITE_ADDR_WIDTH;
-
-      localparam maxSIZE = `max(READ_SIZE, WRITE_SIZE); 
-      localparam maxWIDTH = `max(READ_WIDTH, WRITE_WIDTH); 
-      localparam minWIDTH = `min(READ_WIDTH, WRITE_WIDTH);
-      
-      localparam RATIO = maxWIDTH / minWIDTH; 
-      localparam log2RATIO = log2(RATIO);
+//      `define max(a,b) ((a) > (b) ? (a) : (b))
+//      `define min(a,b) ((a) < (b) ? (a) : (b))
+//
+//      function integer log2; 
+//        input integer value; reg [31:0] shifted; 
+//        integer res; 
+//      begin 
+//        if (value < 2) log2 = value; 
+//        else 
+//          begin 
+//            shifted = value-1;
+//            for (res=0; shifted>0; res=res+1) 
+//              shifted = shifted>>1; 
+//            log2 = res;
+//          end 
+//      end 
+//      endfunction
+//
+//      localparam READ_SIZE  = 2**READ_ADDR_WIDTH;
+//      localparam WRITE_SIZE = 2**WRITE_ADDR_WIDTH;
+//
+//      localparam maxSIZE = `max(READ_SIZE, WRITE_SIZE); 
+//      localparam maxWIDTH = `max(READ_WIDTH, WRITE_WIDTH); 
+//      localparam minWIDTH = `min(READ_WIDTH, WRITE_WIDTH);
+//      
+//      localparam RATIO = maxWIDTH / minWIDTH; 
+//      localparam log2RATIO = log2(RATIO);
+      localparam SIZE  = 2**ADDR_WIDTH;
 		
-		(* ramstyle = "M9K" *) logic [RATIO-1:0][minWIDTH-1:0] ram[0:maxSIZE-1];
+		(* ramstyle = "M9K" *) logic [WIDTH-1:0] ram[0:SIZE-1];
 		
 	   always_ff@(posedge clk)
 		  begin
-			if(w_valid) ram[w_addr / RATIO][w_addr % RATIO] <= w_data;
-			r_data <= ram[r_addr];
+			if(w_valid) ram[w_addr] = w_data;
+			if(r_valid) r_data = ram[r_addr];
 		end
 
 //      (* ramstyle = "M9K" *) reg [minWIDTH-1:0] RAM [0:maxSIZE-1]; 
